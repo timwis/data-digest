0ddc6b851c1bf029a5883d28d65f9e631372e46d
timwis <tim@timwis.com>
2017-03-09 13:22:17 -0500

    server: pass db instance to make tests idempotent closes #7

--- server/server.js
+++ server/server.js
@@ -1,102 +1,110 @@
-const nodeEnv = process.env.NODE_ENV || 'development'
-const dbConfig = require('../knexfile')[nodeEnv]
-const knex = require('knex')(dbConfig)
 const fastify = require('fastify')
 
-const app = fastify()
-module.exports = app
+module.exports = createServer
 
-app.get('/services/:service', async (req, reply) => {
-  try {
-    const service = await getService(req.params.service)
-    reply.send(service)
-  } catch (err) {
-    reply.code(404).send(err)
-  }
-})
+// db -> app
+function createServer (db) {
+  const app = fastify()
 
-app.get('/services/:service/queries', async (req, reply) => {
-  try {
-    const service = await getService(req.params.service)
-    const queries = await knex('queries').where('service_id', service.service_id)
-    reply.send(queries)
-  } catch (err) {
-    reply.code(404).send(err)
-  }
-})
-
-app.get('/services/:service/subscribers', async (req, reply) => {
-  try {
-    const service = await getService(req.params.service)
-    const subscribers = await knex('queries')
-      .where('service_id', service.service_id)
-      .innerJoin('subscribers', 'subscribers.query_id', 'queries.query_id')
-    reply.send(subscribers)
-  } catch (err) {
-    reply.code(404).send(err)
-  }
-})
-
-const postSubscriberSchema = {
-  params: {
-    service: { type: 'string' }
-  },
-  payload: {
-    query: { type: 'string' },
-    email: { type: 'string', format: 'email' }
-  }
-}
-app.post('/services/:service/subscribers', postSubscriberSchema, async (req, reply) => {
-  try {
-    const service = await getService(req.params.service)
-    const existingQueries = await knex('queries')
-      .where('service_id', service.service_id)
-      .where('query', req.body.query)
-    if (existingQueries.length) {
-      const queryId = existingQueries[0].query_id
-      const subscriber = await createSubscriber(queryId, req.body.email)
-      const statusCode = subscriber.created ? 201 : 200
-      const subscriberId = subscriber.subscriber_id
-      reply.code(statusCode).send(`Created subscriber ${subscriberId} for existing query`)
-    } else {
-      const queryId = await knex('queries').insert({
-        service_id: service.service_id,
-        query: req.body.query
-      }).returning('query_id')
-      const subscriber = await createSubscriber(queryId, req.body.email)
-      const statusCode = subscriber.created ? 201 : 200
-      const subscriberId = subscriber.subscriber_id
-      reply.code(statusCode).send(`Created subscriber ${subscriberId} for new query ${queryId}`)
+  app.get('/services/:service', async (req, reply) => {
+    try {
+      const service = await getService(req.params.service)
+      reply.send(service)
+    } catch (err) {
+      reply.code(404).send(err)
     }
-  } catch (err) {
-    reply.code(404).send(err)
+  })
+
+  app.get('/services/:service/queries', async (req, reply) => {
+    try {
+      const service = await getService(req.params.service)
+      const queries = await db('queries').where('service_id', service.service_id)
+      reply.send(queries)
+    } catch (err) {
+      reply.code(404).send(err)
+    }
+  })
+
+  app.get('/services/:service/subscribers', async (req, reply) => {
+    try {
+      const service = await getService(req.params.service)
+      const subscribers = await db('queries')
+        .where('service_id', service.service_id)
+        .innerJoin('subscribers', 'subscribers.query_id', 'queries.query_id')
+      reply.send(subscribers)
+    } catch (err) {
+      reply.code(404).send(err)
+    }
+  })
+
+  const postSubscriberSchema = {
+    params: {
+      service: { type: 'string' }
+    },
+    payload: {
+      query: { type: 'string' },
+      email: { type: 'string', format: 'email' }
+    }
   }
-})
- 
-async function getService (service) {
-  const services = await knex('services').where('slug', service)
-  if (!services.length) throw `Service ${service} not found`
-  else return services[0]
-}
+  app.post('/services/:service/subscribers', postSubscriberSchema, async (req, reply) => {
+    try {
+      const service = await getService(req.params.service)
+      const existingQueries = await db('queries')
+        .where('service_id', service.service_id)
+        .where('query', req.body.query)
+      if (existingQueries.length) {
+        const queryId = existingQueries[0].query_id
+        const subscriber = await createSubscriber(queryId, req.body.email)
+        const statusCode = subscriber.created ? 201 : 200
+        const subscriberId = subscriber.subscriber_id
+        reply.code(statusCode).send(`Created subscriber ${subscriberId} for existing query`)
+      } else {
+        const queryId = await db('queries').insert({
+          service_id: service.service_id,
+          query: req.body.query
+        }).returning('query_id')
+        const subscriber = await createSubscriber(queryId, req.body.email)
+        const statusCode = subscriber.created ? 201 : 200
+        const subscriberId = subscriber.subscriber_id
+        reply.code(statusCode).send(`Created subscriber ${subscriberId} for new query ${queryId}`)
+      }
+    } catch (err) {
+      reply.code(404).send(err)
+    }
+  })
 
-async function createSubscriber (queryId, email) {
-  const existingSubscribers = await knex('subscribers')
-    .where('query_id', queryId)
-    .where('email', email)
+  return app
+   
+  async function getService (service) {
+    const services = await db('services').where('slug', service)
+    if (!services.length) throw `Service ${service} not found`
+    else return services[0]
+  }
 
-  if (existingSubscribers.length) {
-    const subscriberId = existingSubscribers[0].subscriber_id
-    return { subscriber_id: subscriberId, created: false }
-  } else {
-    const subscriberId = await knex('subscribers').insert({
-      query_id: queryId,
-      email: email
-    }).returning('subscriber_id')
-    return { subscriber_id: subscriberId, created: true }
+  async function createSubscriber (queryId, email) {
+    const existingSubscribers = await db('subscribers')
+      .where('query_id', queryId)
+      .where('email', email)
+
+    if (existingSubscribers.length) {
+      const subscriberId = existingSubscribers[0].subscriber_id
+      return { subscriber_id: subscriberId, created: false }
+    } else {
+      const subscriberId = await db('subscribers').insert({
+        query_id: queryId,
+        email: email
+      }).returning('subscriber_id')
+      return { subscriber_id: subscriberId, created: true }
+    }
   }
 }
 
 if (!module.parent) { // Only run if called directly, not within tests
+  const nodeEnv = process.env.NODE_ENV || 'development'
+  const dbConfig = require('../knexfile')[nodeEnv]
+  const db = require('knex')(dbConfig)
+
+  const app = createServer(db)
   const port = process.env.PORT || '8080'
   app.listen(port, (err) => {
     if (err) throw err
