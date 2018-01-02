01e6ac0c252366e09f742049f82b1970dbf8f1c3
timwis <tim@timwis.com>
2018-01-02 14:17:52 -0500

    Use DATABASE_URL env var in production

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,18 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL
   },
 
-  worker_development: {
+  test: {
     client: 'sqlite3',
-    connection: {
-      filename: '../dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: ':memory:'
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
-
-};
+}
