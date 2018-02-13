42af4a4feb9a3b959098cf6b125eb4bf9882f61d
timwis <tim@timwis.com>
2018-02-13 07:14:48 -0500

    Initial client boilerplate integrated with koa

---
+++ web/client/nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  srcDir: 'web/client',
+  head: {
+    title: 'subscribeme',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Monitors an API for new data and sends digest emails to subscribers.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#3B8070' }
+}
