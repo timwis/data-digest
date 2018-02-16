0cde6c3b6ab20898dde68fafbbcd19e20208931f
timwis <tim@timwis.com>
2018-02-16 06:29:46 -0500

    add bulma, site-nav bar

---
+++ web/client/components/SiteNav.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  nav.navbar(
+    role='navigation'
+    aria-label='main navigation'
+  )
+    div.navbar-brand
+      nuxt-link.navbar-item(to='/') subscribeme
+      div.navbar-burger
+        span
+        span
+        span
+
+    div.navbar-menu
+      div.navbar-start
+        nuxt-link.navbar-item(to='/') Home
+
+      div.navbar-end
+        // If logged in
+        div.navbar-item.has-dropdown.is-hoverable(
+          v-if='user.nickname'
+        )
+          a.navbar-link {{ user.nickname }}
+          div.navbar-dropdown
+            nuxt-link.navbar-item(to='/logout') Logout
+
+        // If not logged in
+        nuxt-link.navbar-item(
+          v-else
+          to='/login'
+        ) Login
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
