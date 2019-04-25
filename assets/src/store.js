7f04ca99f6523f13269dd4c9e04177229ce744c3
timwis <tim@timwis.com>
2019-04-25 15:31:52 +0100

    Persist state to localStorage

--- assets/src/store.js
+++ assets/src/store.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { getField, updateField } from 'vuex-map-fields'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: true,
+  plugins: [createPersistedState({
+    paths: ['user', 'digest']
+  })],
   state: {
     user: {},
     digest: {
